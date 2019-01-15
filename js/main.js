02aabd10d597dba07acfc4f60b66c2bd141dbf39
Michael King-Stockton <kingstockton@gmail.com>
2019-01-15 09:27:07 -0600

    save to localStorage

--- js/main.js
+++ js/main.js
@@ -3,4 +3,17 @@ document.getElementById('myForm').addEventListener('submit', saveBookmark);
 function saveBookmark() {
   event.preventDefault()
 
+  var siteName = document.getElementById('siteName').value;
+  var siteUrl = document.getElementById('siteUrl').value;
+
+  var bookmark = {
+    name: siteName,
+    url: siteUrl
+  }
+
+localStorage.setItem('test', 'Hello World')
+console.log(localStorage.getItem('test'));
+
+ 
+
 }
