e40108efed457334711791718ec471ce6d33355f
Michael King-Stockton <kingstockton@gmail.com>
2019-01-15 14:40:40 -0600

    more javascript

--- js/main.js
+++ js/main.js
@@ -6,14 +6,41 @@ function saveBookmark() {
   var siteName = document.getElementById('siteName').value;
   var siteUrl = document.getElementById('siteUrl').value;
 
-  var bookmark = {
-    name: siteName,
-    url: siteUrl
+  var bookmark = {name: siteName, url: siteUrl}
+
+// localStorage.setItem('test', 'Hello World')
+// console.log(localStorage.getItem('test'))
+// localStorage.removeItem('test');
+// console.log(localStorage.getItem('test'))
+
+
+// test if bookmarks is null //
+if (localStorage.getItem('bookmark') !== null) {
+  // init array
+  var bookmarksArray = [];
+  // add to array
+  bookmarksArray.push(bookmark);
+  // set to localStroage
+  localStorage.setItem('bookmark', JSON.stringify(bookmarksArray))
+  } else {
+    // get bookmarks from local storage
+    var bookmarksArray = JSON.parse(localStorage.getItem('bookmark'));
+    // add bookmarks to array
+    bookmarksArray.push(bookmark);
+    // Re-set back to localStorage
+      localStorage.setItem('bookmark', JSON.stringify(bookmarksArray))
   }
 
-localStorage.setItem('test', 'Hello World')
-console.log(localStorage.getItem('test'));
 
- 
+
 
 }
+
+window.onload = function fetchBookmakrs() {
+    var bookmarksArray = JSON.parse(localStorage.getItem('bookmark'));
+
+    // get output id
+    var bookmarksResults = document.getElementById('bookmarksResults');
+    // Build output
+    bookmarksResults.innerHTML = 'Hello'
+  }
