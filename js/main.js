cd4a7bd6fda8e6499eb1abe703606d75b7abb143
Michael King-Stockton <kingstockton@gmail.com>
2019-01-18 11:51:04 -0700

    add delete bookmarks

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function saveBookmark() {
 
 
 // test if bookmarks is null //
-if (localStorage.getItem('bookmark') !== null) {
+if (localStorage.getItem('bookmark') === null) {
   // init array
   var bookmarksArray = [];
   // add to array
@@ -30,17 +30,36 @@ if (localStorage.getItem('bookmark') !== null) {
     // Re-set back to localStorage
       localStorage.setItem('bookmark', JSON.stringify(bookmarksArray))
   }
-
-
-
-
 }
 
-window.onload = function fetchBookmakrs() {
+//Delete Bookmark
+function deleteBookmark(url) {
+  //Get bookmarks from localstorage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  for(var i = 0; i < bookmarks.length; i++){
+    if (bookmarks[i].url == url){
+      // Remove from array
+      bookmarks.splaice(i, 1)
+    }
+  }
+}
+
+function fetchBookmarks() {
     var bookmarksArray = JSON.parse(localStorage.getItem('bookmark'));
 
     // get output id
     var bookmarksResults = document.getElementById('bookmarksResults');
     // Build output
-    bookmarksResults.innerHTML = 'Hello'
-  }
+    bookmarksResults.innerHTML = 'Hello';
+
+    for (var i = 0; i < bookmarksArray.length; i++) {
+      var name = bookmarksArray[i].name;
+      var url = bookmarksArray[i].url;
+      bookmarksResults.innerHTML += '<div class = "well">' +  
+                                    '<h3>' + name +
+                                    ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> ' +
+                                    ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="'+url+'">Delete</a> ' +
+                                    '</h3> ' +
+                                    '</div>';
+    }
+ }
